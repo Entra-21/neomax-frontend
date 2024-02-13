8e94f8b60b39d13fcc1e20784bb7f320078ccde7
Matheus Eduardo Beloli <beloli2005@gmail.com>
2024-02-13 14:11:39 -0300

    criado os componentes de loading para os modos dark e light ,ao alterar o tamanho da fonte, o dumbell decentrealizou um pouco, mas depois que verem acho que é fácil arrumar

---
+++ src/app/loading-dark/loading-dark.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-dark',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading-dark.component.html',
+  styleUrl: './loading-dark.component.css'
+})
+export class LoadingDarkComponent {
+
+}
---
+++ src/app/loading-light/loading-light.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-light',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading-light.component.html',
+  styleUrl: './loading-light.component.css'
+})
+export class LoadingLightComponent {
+
+}
