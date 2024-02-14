316f29a17062cb7e2265af58069955a7fd9f5992
Nill Junior <nill-junior@outlook.com>
2024-02-14 13:51:31 -0300

    Add footer routing animations and services

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 
 export class FooterComponent {
 
+  routeSelected: string = 'home'
+
+  constructor(private router: Router) { }
+
+
+
+  navigateToRoute(route: string) {
+    this.router.navigate([`${route}`]);
+    if (this.routeSelected !== route) {
+      this.routeSelected = route;
+    }
+  }
 }
