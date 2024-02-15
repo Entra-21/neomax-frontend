eedd09dad9d8c4ccec82284b2e35c6515c2c12d2
Nill Junior <nill-junior@outlook.com>
2024-02-15 16:35:35 -0300

    Update images folder and delete svg files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoadingLightComponent } from './loading-light/loading-light.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, LoadingLightComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
