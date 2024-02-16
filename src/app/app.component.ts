6235f9d1ee1aebbb585ffa99158cb4800e020dcc
Matheus Eduardo Beloli <146236152+beloli1212@users.noreply.github.com>
2024-02-16 17:46:35 -0300

    9 componente logo loading (#15)
    * criado os componentes de loading para os modos dark e light ,ao alterar o tamanho da fonte, o dumbell decentrealizou um pouco, mas depois que verem acho que é fácil arrumar
    
    * Update images folder and delete svg files
    
    ---------
    
    Co-authored-by: Nill Junior <nill-junior@outlook.com>
    Co-authored-by: Nill Junior <89917303+nilljr@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoadingLightComponent } from './loading-light/loading-light.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-
-  imports: [RouterOutlet, SearchBarComponent,FooterComponent],
+  imports: [RouterOutlet, FooterComponent, LoadingLightComponent, SearchBarComponent]
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
