99c1c472c3cba5be867b2038e4c8f9fa5d85c504
Matheus Eduardo Beloli <146236152+beloli1212@users.noreply.github.com>
2024-02-16 17:44:19 -0300

    6 componente search bar (#16)
    * search bar, inclui a lupa como background, achei que parece muito opaca, tentei buscar como adicionar ela como imagem mesmo porém não encontrei muita coisa, pelo menos que funcionasse visualmente aqui
    
    * search -Matheus
    
    * Adjusted search input and button positions
    
    ---------
    
    Co-authored-by: Nill Junior <nill-junior@outlook.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+
+  imports: [RouterOutlet, SearchBarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
