78c80c4cd9e30d8a215b83d8af3d615d67322a12
Nill Junior <nill-junior@outlook.com>
2024-02-18 23:04:33 -0300

    Remove loading dark component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { LoadingLightComponent } from './loading-light/loading-light.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, LoadingLightComponent, SearchBarComponent]
+  imports: [RouterOutlet, FooterComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
