4ad3dba51748f6f2f4ceb1db8b4434cf93f7428f
Nill Junior <89917303+nilljr@users.noreply.github.com>
2024-02-19 21:18:20 -0300

    18 dark theme (#21)
    * Add dark theme for current pages, create theme toggle and adjusted footer size
    
    * Create dark theme for routines page.

--- src/app/components/add-button/add-button.component.ts
+++ src/app/components/add-button/add-button.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './add-button.component.html',
   styleUrl: './add-button.component.css'
 })
 export class AddButtonComponent {
+
+  constructor(public api: ApiService) { }
 
 }
